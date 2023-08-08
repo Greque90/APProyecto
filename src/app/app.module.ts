bf1e1cda756eab88644da72fdd3bee3a89df8b41
Greque90 <rodriguezamalia241@gmail.com>
2023-08-07 21:37:48 -0300

    PFinal

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { EncabezadoComponent } from './Componentes/1encabezado/encabezado.component';
+import { InicioComponent } from './Componentes/2inicio/inicio.component';
+import { NosotrosComponent } from './Componentes/3nosotros/nosotros.component';
+import { ServiciosComponent } from './Componentes/4servicios/servicios.component';
+import { ContactoComponent } from './Componentes/5contacto/contacto.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EncabezadoComponent,
+    InicioComponent,
+    NosotrosComponent,
+    ServiciosComponent,
+    ContactoComponent
   ],
   imports: [
     BrowserModule
